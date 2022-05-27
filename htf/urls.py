dbab84f1fd425adde7e1ea6f2035a4377c141019
hearthacker19 <106328546+hearthacker19@users.noreply.github.com>
2022-05-27 09:43:31 +0530

    Update urls.py

--- htf/urls.py
+++ htf/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("hacking.urls"))
+    path("/", include("hacking.urls"))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
